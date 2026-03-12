1808d9fab41dfbbe436c556231e4982501013534
Gabriel-7866 <gn634818@gmail.com>
2026-03-11 21:23:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 function comprar(livro){
 
-let lista = document.getElementById("meusLivros");
-
-let item = document.createElement("li");
-
-item.innerText = livro + " (comprado)";
-
-lista.appendChild(item);
-
-alert("Livro adicionado à sua biblioteca!");
+alert("Em breve você poderá comprar o ebook: " + livro);
 
 }
